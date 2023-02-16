75219a679c065ccdbf48f31ee212a9657bd0818b
lucas laurencich <lucas.maximiliano89@gmail.com>
2023-02-16 10:31:52 +0100

    JS Packages Day

---
+++ JavascriptPackages/javascript-packages-boilerplate/src/plugins/sortable.js
@@ -0,0 +1,22 @@
+// we can delete this file as the stimulus contoller now takes care of all Javascript
+
+
+// import Sortable from 'sortablejs';
+
+// const list = document.querySelector("#results");
+
+// const initSortable = () => {
+//   Sortable.create(list, {
+//     ghostClass: "ghost",
+//     animation: 150,
+//     onEnd: (event) => {
+//       alert(`${event.oldIndex} moved to ${event.newIndex}`)
+//     }
+//   })
+// };
+
+// export {initSortable}
+
+
+
+
